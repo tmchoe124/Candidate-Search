996d95cf533bfe047804f55ace3f96a698578cd3
tmchoe124 <tmchoe@yahoo.com>
2025-02-03 17:42:33 -0800

    updated port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+  },
 });
